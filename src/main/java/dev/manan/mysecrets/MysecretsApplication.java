4c0d3e36f647243fdcaf9d3d4ba0ec4dae2eaddf
Manan Solanki <mananrsolanki@gmail.com>
2023-09-27 00:54:56 +0530

    Project init

---
+++ src/main/java/dev/manan/mysecrets/MysecretsApplication.java
@@ -0,0 +1,13 @@
+package dev.manan.mysecrets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysecretsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysecretsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/manan/mysecrets/MysecretsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.manan.mysecrets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysecretsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
